ba17cf09c11d4265bb0fbf4640dbf89c9b5f45b5
Gabriel de Barros Paranhos da Costa <gabrielbpcosta@gmail.com>
2016-09-15 08:07:19 -0300

    Initial commit

---
+++ googlecalendar.py
@@ -0,0 +1,83 @@
+import os
+import datetime
+import httplib2
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+class GoogleCalendarBot(object):
+    calendar = None
+    client_secret_file = None
+
+    # TODO: modify constants
+    SCOPES = 'https://www.googleapis.com/auth/calendar'
+    APPLICATION_NAME = 'WikiCFP 2 Google Calendar'
+
+    def __init__(self, calendar='primary', client_secret_file = 'client_secret.json'):
+        self.calendar = calendar
+        self.client_secret_file = client_secret_file
+
+    def get_credentials(self):
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'wikicfp2calendar.json')
+
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(self.client_secret_file, self.SCOPES)
+            flow.user_agent = self.APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else: # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
+
+    def listCalendars(self):
+        credentials = self.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
+
+        page_token = None
+        while True:
+          calendar_list = service.calendarList().list(pageToken=page_token).execute()
+          for calendar_list_entry in calendar_list['items']:
+            print "%s : %s" % (calendar_list_entry['summary'], calendar_list_entry['id'])
+          page_token = calendar_list.get('nextPageToken')
+          if not page_token:
+            break
+
+    def setCalendar(self, calendar):
+        self.calendar = calendar
+
+    def includeEvents(self, events):
+        credentials = self.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
+
+        # for event in events:
+        event = events[0]
+        print event
+        event = service.events().insert(calendarId=self.calendar, body=event).execute()
+        if verbose > 0:
+            print 'Event created: %s' % (event.get('htmlLink'))
---
+++ wikicfp.py
@@ -0,0 +1,85 @@
+import feedparser
+import urllib
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+
+class WikiCFPParser(object):
+    categories = None
+    last_update = NotImplemented
+    feed_url = 'http://www.wikicfp.com/cfp/rss?cat=%s'
+
+    def __init__(self, categories):
+        self.categories = categories
+        # TODO: include last_update (read from file)
+
+    def prepareCategory(self, category):
+        return category.lower().replace(' ', '%20')
+
+    def parseFeed(self, verbose = 0):
+        for category in self.categories:
+            feed = feedparser.parse(self.feed_url % self.prepareCategory(category))
+
+            # TODO: Check if it was updated (compare previous update date to d.updated ou d.updated_parsed)
+
+            events = []
+            n_entries = len(feed.entries)
+            for ii, entry in enumerate(feed.entries):
+                if verbose > 0:
+                    print "Processing entry %d / %d from the %s category" % (ii+1, n_entries, category.capitalize())
+
+                title = entry.title_detail.value.split(' : ')[0] # Acronym
+                description = entry.summary # Full description
+                link = entry.link # URL
+
+                conf_page_data = urllib.urlopen(link) # capture page data using URL to WikiCFP page of the conference
+
+                soup = BeautifulSoup(conf_page_data, "html.parser")
+
+                table = soup.body.find_all('table', attrs={'class': 'gglu'})[0] # Select table with important dates and where the event will occur
+                tds = table.find_all('td')
+                dates_text = "Important information:\n" # text that will be included in the event description
+                for jj, tp in enumerate(table.find_all('th')):
+                    dates_text = dates_text + "\t" + tp.text.strip() + ": " + tds[jj].text.strip() + "\n"
+
+                    if tp.text.strip().lower() == "submission deadline":
+                        deadline = datetime.strptime(tds[jj].text.strip(), '%b %d, %Y') # Deadline will be used as event date
+                    if tp.text.strip().lower() == "where":
+                        location = tds[jj].text.strip() # Location will be used in event
+
+                categories_table = soup.body.find_all('table', attrs={'class': 'gglu'})[1]
+                keywords = categories_table.find_all('a')[1:]
+                keywords_list = []
+                keywords_text = "Keywords:\n"
+                for keyword in keywords:
+                    keywords_list.append(keyword.text.capitalize())
+                    keywords_text = keywords_text + "\t" + keyword.text.capitalize() + "\n"
+
+                final_description = description + "\n\n" + dates_text + "\n" + keywords_text
+
+                event = {'summary': title,
+                        'description': final_description,
+                        'location': location,
+                        'start': {
+                            'date': deadline.strftime("%Y-%m-%d")
+                            },
+                        'end': {
+                            'date': deadline.strftime("%Y-%m-%d")
+                            },
+                        'endTimeUnspecified': True,
+                        'anyoneCanAddSelf': True,
+                        'guestsCanInviteOthers': True,
+                        'guestsCanModify': True,
+                        'reminders': {
+                            'useDefault': True
+                            }
+                        }
+
+                if datetime.today().date() < deadline.date() and event not in events:
+                    # print deadline.strftime("%b %d, %Y")
+                    events.append(event)
+
+                time.sleep(5) # WikiCFP requested that, at most, one query was issued every five seconds
+            events = sorted(events, key = lambda tup: tup['start']['date'])
+
+        return events
